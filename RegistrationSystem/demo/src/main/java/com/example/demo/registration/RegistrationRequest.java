f4eddbf1e84c551a4b8eda68f1f8fb9a1314d835
Joan Canossa <76176196+jfr4nc0@users.noreply.github.com>
2021-09-17 18:03:56 -0300

    Add files via upload

---
+++ RegistrationSystem/demo/src/main/java/com/example/demo/registration/RegistrationRequest.java
@@ -0,0 +1,30 @@
+package com.example.demo.registration;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+
+	private final String firstName = "";
+	private final String lastName = "";
+	private final String email = "";
+	private final String password = "";
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getPassword() {
+		return password;
+	}
+}
